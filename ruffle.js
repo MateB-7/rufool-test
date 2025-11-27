8a24360f631b498114b69568d423e6140ff5a981
MateB-7 <mateobarroso@protonmail.com>
2025-11-27 11:40:52 +1100

    Update ruffle.js

--- ruffle.js
+++ ruffle.js
@@ -1,1 +1,12 @@
-<script src="https://unpkg.com/@ruffle-rs/ruffle"></script>
+var swfobject = {};
+
+swfobject.embedSWF = function(url, cont, width, height){
+    var ruffle = window.RufflePlayer.newest(),
+        player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
+            width: width,
+            height: height,
+            style: 'width: ' + width + 'px; height: ' + height + 'px',
+        });
+
+    player.load({ url: url });
+}
